14c22a1694bb403c71ed64a2dfba348b3f68febb
chafik-akmouche <chafik.akmouch@gmail.com>
2021-11-03 12:17:15 +0100

    resolution conflit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,17 @@ import { PlanningService } from './services/planning.service';
 import { PlanningViewComponent } from './planning-view/planning-view.component';
 import { CsvReader } from './services/csvReader.service';
 import { HttpClientModule } from '@angular/common/http';
+import { ConfigurationComponent } from './configuration/configuration.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+
+const appRoutes : Routes = [
+  { path: '', component: PlanningViewComponent },
+  { path: 'configuration', component: ConfigurationComponent },
+  { path: 'connexion', component: PlanningViewComponent },
+  { path: 'not-found', component: FourOhFourComponent },
+  { path: '**', redirectTo: 'not-found' }
+];
 
 @NgModule({
   declarations: [
@@ -20,19 +31,22 @@ import { HttpClientModule } from '@angular/common/http';
     PlanningComponent,
     NavbarComponent,
     PlanningFilterComponent,
-    PlanningViewComponent
+    PlanningViewComponent,
+    ConfigurationComponent,
+    FourOhFourComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     PlanningFilterService,
     PlanningService,
     CsvReader,
-    
+
   ],
   bootstrap: [AppComponent]
 })
